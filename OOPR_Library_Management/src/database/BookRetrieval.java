a17286c9eb67a88bc56a2516680d5b6db4e3adbd
underlinear <arduouschore@gmail.com>
2024-05-24 21:03:01 +0800

    modify reservation to retrieve 0 quantity book

--- OOPR_Library_Management/src/database/BookRetrieval.java
+++ OOPR_Library_Management/src/database/BookRetrieval.java
@@ -1,4 +1,5 @@
 package database;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -6,16 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookRetrieval extends MySQLConnection {
-    private static final int TOLERANCE_LEVEL = 4; // Adjust this value as needed
-    private String search = "";
+    // private static final int TOLERANCE_LEVEL = 4; // Adjust this value as needed
+    // private String search = "";
 
-    public BookRetrieval(String search){
-        this.search = search;
-    }
+    // public BookRetrieval(String search){
+    //    this.search = search;
+    // }
     
-    public BookRetrieval(){
-        this.search = "";
-    }
+    // public BookRetrieval(){
+    //    this.search = "";
+    // }
 
     public List<Book> fetchBooksFromDatabase() throws SQLException {
         List<Book> books = new ArrayList<>();
@@ -58,6 +59,31 @@ public class BookRetrieval extends MySQLConnection {
         }
         return books;
     }
+    
+    public List<Book> fetchBooksWithZeroAvailableQuantity() throws SQLException {
+        List<Book> books = new ArrayList<>();
+        String query = "SELECT author, title, quantity, category, available_quantity, date_published, book_id " +
+                       "FROM Books " +
+                       "WHERE available_quantity = 0";
+        
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String author = resultSet.getString("author");
+                String title = resultSet.getString("title");
+                String datePublished = resultSet.getString("date_published");
+                String category = resultSet.getString("category");
+                int quantity = resultSet.getInt("quantity");
+                int availableQuantity = resultSet.getInt("available_quantity");
+                int book_id = resultSet.getInt("book_id");
+                books.add(new Book(author, title, datePublished, category, quantity, availableQuantity, book_id));
+            }
+        }
+        return books;
+    }
+
+
     /*
     // Unused
 
